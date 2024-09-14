d914e87256c9eca80819b8a31b48ed9a2e754cbf
Caceresenzo <caceresenzo1502@gmail.com>
2024-09-14 15:23:18 +0200

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -1,15 +1,12 @@
-import socket  # noqa: F401
+import socket
+
+PORT = 9092
 
 
 def main():
-    # You can use print statements as follows for debugging,
-    # they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
-    # server = socket.create_server(("localhost", 9092), reuse_port=True)
-    # server.accept() # wait for client
+    print(f"listen: {PORT}")
+    server = socket.create_server(("localhost", PORT), reuse_port=True)
+    server.accept()
 
 
 if __name__ == "__main__":
